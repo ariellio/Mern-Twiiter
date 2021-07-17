763975f420cdee0af4a6c281850f97f7729813b8
adavies <arielpdavies@gmail.com>
2021-07-16 23:04:08 -0400

    created model with a post req

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
-const port = process.env.PORT || 5000;
+const db = require('./config/keys').mongoURI;
 
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => res.send("Hello wwworld!"));
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-app.get("/", (req, res) => res.send("Hello orld!"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 
 
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
+  .then(() => console.log("Connected to MongoDB successfully!"))
   .catch(err => console.log(err));
