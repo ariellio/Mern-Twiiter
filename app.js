a7e651122c3449a3ec548b3044372b0cb65dafd2
adavies <arielpdavies@gmail.com>
2021-07-17 09:26:08 -0400

    login with tokens working

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require("express");
-const app = express();
 const mongoose = require('mongoose');
+const path = require("path");
 const db = require('./config/keys').mongoURI;
+const app = express();
 
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.get("/", (req, res) => res.send("Hello wwworld!"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
 
 
 
